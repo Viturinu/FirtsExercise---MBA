01118f263431b812b3ddc2b7cb072f24766c41a3
Victor Oliveira Almeida <victor.almeida.ti@gmail.com>
2024-12-13 17:54:47 -0300

    first commit

---
+++ javascript/effects.js
@@ -0,0 +1,10 @@
+const searchInput = document.getElementById("input-wrapper");
+
+searchInput.addEventListener("click", () => {
+    alert("teste0");
+    searchInput.style.border = "2px solid #45b442"; // Substitua #45b442 pela cor desejada
+});
+
+searchInput.addEventListener("blur", () => {
+    searchInput.style.border = "1px solid #d3cfd2"; // Remove a borda quando o campo perde o foco
+});
